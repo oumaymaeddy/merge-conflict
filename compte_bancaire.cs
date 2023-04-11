fe321550fb65363e6bbf1f3c705ccde0ca318efe
oumayma <oumayma@oumaymas-MacBook-Air.local>
2023-04-11 01:52:08 -0400

    generer le ID aleatoirement

--- compte_bancaire.cs
+++ compte_bancaire.cs
@@ -7,17 +7,20 @@ namespace appBancaire
         public string Nom;
         public string Prenom;
         public int Numero_telephone;
+        public string Adresse;
+
         
 
-        public compte_bancaire(string nom, string prenom, int numero_telephone)
+        public compte_bancaire(string nom, string prenom, int numero_telephone,string adresse)
         {
             
-            id=number;
+             Random rand = new Random();
+            int NUMBER = rand.Next(100000, 999999);
+            Id = $"001{NUMBER:D6}";
             Nom = nom;
             Prenom = prenom;
             Numero_telephone = numero_telephone;
            
-
 
         }
 
