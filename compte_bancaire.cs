5342fee50f780d55cfba78d613f08681089ec2f7
oumayma <oumayma@oumaymas-MacBook-Air.local>
2023-04-11 01:11:04 -0400

    ajout du classe

---
+++ compte_bancaire.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace appBancaire
+{
+    public class compte_bancaire
+    {
+        public string Id;
+        public string Nom;
+        public string Prenom;
+        public int Numero_telephone;
+        public string Adresse;
+
+        public compte_bancaire(string nom, string prenom, int numero_telephone, string adresse)
+        {
+            
+            Random rand = new Random();
+            int NUMBER = rand.Next(100000, 999999);
+            Id = $"001{NUMBER:D6}";
+            
+            Nom = nom;
+            Prenom = prenom;
+            Numero_telephone = numero_telephone;
+            Adresse = adresse;
+
+
+        }
+
+
+
+    }
+}
+
