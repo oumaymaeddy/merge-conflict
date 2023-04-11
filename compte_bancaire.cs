466672d1544faed182bbf5657b053e91c337b46b
oumayma <oumayma@oumaymas-MacBook-Air.local>
2023-04-11 01:59:45 -0400

    amelioration du code

--- compte_bancaire.cs
+++ compte_bancaire.cs
@@ -7,18 +7,40 @@ namespace appBancaire
         public string Nom;
         public string Prenom;
         public int Numero_telephone;
+        public string Adresse;
+        Adresse = adresse;
+
         
 
-        public compte_bancaire(string nom, string prenom, int numero_telephone)
+        public compte_bancaire(string nom, string prenom, int numero_telephone,  string adresse)
         {
             
-            id=number;
+            Random rand = new Random();
+            int NUMBER = rand.Next(1000000, 9999999);
+            Id = NUMBER:D6;
             Nom = nom;
             Prenom = prenom;
             Numero_telephone = numero_telephone;
            
 
 
+        }
+
+        
+        public void changement_adresse(string nv_adresse)
+        {
+
+            Adresse = nv_adresse;
+
+        }
+        public void Afficher_MonCompte()
+        {
+            Console.WriteLine("bonjour " + Nom + " " + Prenom + " voici vos informations: ");
+            Console.WriteLine("votre id du compte est " + Id);
+            Console.WriteLine("votre numero de telephone est " + Numero_telephone);
+            Console.WriteLine("votre adresse est" + Adresse);
+
+            
         }
 
 
